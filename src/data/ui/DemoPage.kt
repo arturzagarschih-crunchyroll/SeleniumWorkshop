64d2d276c44fd33087e54afeea709380a6ea377d
Maxim Savitchi <maxim.savitchi@ellation.com>
2023-03-13 18:24:31 +0200

    Extracted page

---
+++ src/data/ui/DemoPage.kt
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+
+class DemoPage(driver: WebDriver) {
+
+	val firstName = driver.findElement(By.name("first_name"))
+
+	val lastName = driver.findElement(By.name("last_name"))
+
+	val companyName = driver.findElement(By.name("business_name"))
+
+	val email = driver.findElement(By.name("email"))
+
+	val submitButton = driver.findElement(By.id("demo"))
+
+	val captcha = driver.findElement(By.xpath("//div[contains(@class, 'row fcr')]//h2"))
+
+	val captchaInput = driver.findElement(By.id("number"))
+
+	fun calculateCaptcha(): Int {
+		val captchaValue = captchaInput.text
+		//val regex = "(\\d+)\\s+([+\\-*/]+)\\s+(\\d+)".toRegex()
+		val regex = "(?<val1>\\d+)\\s+(?<sign>[+\\-*/]+)\\s+(?<val2>\\d+)".toRegex()
+		val match = regex.find(captcha.text)!!
+		val val1 = match.groups["val1"]!!.value.toInt()
+		val val2 = match.groups["val2"]!!.value.toInt()
+		val sign = match.groups["sign"]!!.value
+
+		println(val1)
+		println(val2)
+		println(sign)
+		val captchaResult = when (sign) {
+			"+" -> val1 + val2
+			"-" -> val1 - val2
+			"*" -> val1 * val2
+			"/" -> val1 / val2
+			else -> {
+				throw NumberFormatException()
+			}
+		}
+		println(captchaResult)
+		return captchaResult
+	}
+}
--- src/test/java/SeleniumTests.kt
+++ src/test/java/SeleniumTests.kt
@@ -13,43 +13,21 @@ class SeleniumTests {
 	@Test
 	fun test() {
 		driver.get("https://phptravels.com/demo")
-		driver.findElement(By.name("first_name")).sendKeys("MyTestName")
-		driver.findElement(By.name("last_name")).sendKeys("MyLastTestName")
-		driver.findElement(By.name("email")).sendKeys("testemail@email.com")
-		driver.findElement(By.id("demo")).click()
+		val demoPage = DemoPage(driver)
+		demoPage.firstName.sendKeys("MyTestName")
+		demoPage.lastName.sendKeys("MyLastTestName")
+		demoPage.email.sendKeys("testemail@email.com")
+		demoPage.submitButton.click()
 		var alert = driver.switchTo().alert()
 		println(alert.text)
 		alert.accept()
-		driver.findElement(By.name("business_name")).sendKeys("Company")
-		driver.findElement(By.id("demo")).click()
+		demoPage.companyName.sendKeys("Company")
+		demoPage.submitButton.click()
 		alert = driver.switchTo().alert()
 		println(alert.text)
 		alert.accept()
-		val capcha = driver.findElement(By.xpath("//div[contains(@class, 'row fcr')]//h2")).getText().trim() //text
-		println(capcha)
-		//val regex = "(\\d+)\\s+([+\\-*/]+)\\s+(\\d+)".toRegex()
-		val regex = "(?<val1>\\d+)\\s+(?<sign>[+\\-*/]+)\\s+(?<val2>\\d+)".toRegex()
-		val match = regex.find(capcha)!!
-		val val1 = match.groups["val1"]!!.value.toInt()
-		val val2 = match.groups["val2"]!!.value.toInt()
-		val sign = match.groups["sign"]!!.value
-
-		println(val1)
-		println(val2)
-		println(sign)
-		val captcha = when (sign) {
-			"+" -> val1 + val2
-			"-" -> val1 - val2
-			"*" -> val1 * val2
-			"/" -> val1 / val2
-			else -> {
-				throw NumberFormatException()
-			}
-		}
-		println(captcha)
-		driver.findElement(By.id("number")).sendKeys(captcha.toString())
-		driver.findElement(By.id("demo")).click()
-
+		demoPage.captchaInput.sendKeys(demoPage.calculateCaptcha().toString())
+		demoPage.submitButton.click()
 		Assert.assertTrue("Completed sign is present", driver.findElements(By.xpath("//div[@class='completed']")).size > 0)
 	}
 
